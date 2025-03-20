a18899366b2b60d967a7d747111aec0c6730f215
makstsar17 <makstsar1701@gmail.com>
2025-03-20 12:15:30 +0200

    refactor: include user data in comments-related response

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,6 +1,8 @@
 import { HTTP_CODES } from "../constants/httpCodes";
 import { CommentService } from "../services/CommentService";
+import { ServiceCommentModel } from "../services/models/ServiceCommentModel";
 import { PostService } from "../services/PostService";
+import { UserService } from "../services/UserService";
 import { CreateCommentModel } from "./models/CreateCommentModel";
 import { ResponseCommentModel } from "./models/ResponseCommentModel";
 import { URIParamsCommentIdModel } from "./models/URIParamsCommentIdModel";
@@ -17,8 +19,8 @@ export const commentController = {
         }
         try {
             const comment = await CommentService.createComment(req.body.content, req.body.postId, req.user!.id);
-
-            res.status(HTTP_CODES.CREATED).send(comment);
+            const result = await addAdditionalInfo(comment);
+            res.status(HTTP_CODES.CREATED).send(result);
         } catch (err) {
             res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).send({
                 error: "Server error"
@@ -50,6 +52,23 @@ export const commentController = {
             res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).send({
                 error: "Server error"
             });
+        }
+    }
+}
+
+async function addAdditionalInfo(comment: ServiceCommentModel): Promise<ResponseCommentModel> {
+    const user = await UserService.getUserById(comment.userId);
+    if (!user) {
+        throw new Error(`Can't find user with id ${comment.userId}`);
+    }
+    const { userId, ...result } = comment;
+    return {
+        ...result,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatarUrl: user.avatarUrl
         }
     }
 }
